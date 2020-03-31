60912d0792466159f45ce8eeb08b95125e0dd6ad
Daniil <axsell232@gmail.com>
2020-03-31 18:12:48 +0700

    Add main func, flipping questions

--- src/script.js
+++ src/script.js
@@ -1,11 +1,62 @@
-let elemt = document.getElementsByClassName('buttonStartGame')[0];
+const buttonStartGame = document.getElementById('buttonStartGame');
+const questionsPlace = document.getElementById('questionsPlace');
+const numberTheme = document.getElementsByClassName('buttonMenu').length;
+const numberButtonNext = document.getElementsByClassName('buttonNext').length;
+let globalCount = 0;
+let themeClick = [];
+let buttonNext = [];
+let count = 0;
 
-elemt.addEventListener("click", start);
+for (let i = 0; i < numberTheme; i++) // Инициализация всех тем
+    themeClick[i] = document.getElementsByClassName('buttonMenu')[i];
 
-function start()
+for (let i = 0; i < numberButtonNext; i++) // Назначение кнопок "ДАЛЕЕ" перехода между вопросами
+    buttonNext[i] = document.getElementsByClassName('buttonNext')[i];
+
+function start() // Старт игры
 {
-    let elem = document.getElementsByClassName('contentMenu')[0];
-    let startWindow = document.getElementsByClassName('startWindow')[0];
-    elem.style.display = 'block';
-    startWindow.style.display = 'none';
+    document.getElementsByClassName('contentMenu')[0].style.display = 'block';
+    document.getElementsByClassName('startWindow')[0].style.display = 'none';
 }
+
+function testQuestion(questions, arrayAnswer, correctAnswer)
+{
+    let textQuestion = document.createElement('p');
+    textQuestion.textContent = arrayAnswer[0];
+    questionsPlace.append(textQuestion);
+}
+
+function initialQuestion(questions, themeQuestion) 
+{
+    for (let i = 0; i < 15; i++)
+        questions[i] = document.getElementsByClassName(themeQuestion)[i];
+}
+
+let themeProg = {
+    progCount: 0,
+    questions: [],
+}
+
+let theme = [themeProg];
+
+
+function flippingQuestions(theme)
+{
+    if (count > 0)
+        theme.questions[count - 1].style.display = 'none';
+    theme.questions[count].style.display = 'block';  
+    count++;
+}
+
+function mainGame(theme)
+{
+    initialQuestion(theme.questions, 'questionProg');
+    document.getElementsByClassName('contentMenu')[0].style.display = 'none';
+    buttonNext[0].style.display = 'block';
+    flippingQuestions(theme);
+}
+
+
+buttonStartGame.addEventListener('click', start);
+themeClick[0].addEventListener('click', () => mainGame(theme[0]));
+buttonNext[0].addEventListener('click', () => flippingQuestions(theme[0])); 
